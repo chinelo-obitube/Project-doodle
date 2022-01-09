b03bbfd3e6b1a0822c037d364624385ebbd2d84f
chinelo-obitube <obitubec@gmail.com>
2022-01-09 16:10:53 +0100

    Modified __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY'] = 'chinelo chinelo'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
