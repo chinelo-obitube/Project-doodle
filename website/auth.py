b884f2e0a3d3dcb2ec88639a47c28d423d8fe6e9
chinelo-obitube <obitubec@gmail.com>
2022-01-09 17:25:25 +0100

    Made some changes

--- website/auth.py
+++ website/auth.py
@@ -17,13 +17,13 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
+                flash('Logged in successfully!, Welcome', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('Email does not exist. Please try again', category='error')
 
     return render_template("login.html", user=current_user)
 
